e59770280de369ab43d1741e1a43a046898d5fbe
Esteban Leon <eleon182@gmail.com>
2018-08-09 16:56:59 -0700

    init game

--- service/game.js
+++ service/game.js
@@ -2,13 +2,44 @@ var async = require('async');
 var lo = require('lodash');
 
 module.exports = {
-	startNewGame: startNewGame
+	initGame: initGame,
+	name: name,
 };
 
-var game = {};
+var game;
 
+initGame();
 
-function startNewGame() {
-
+console.log(game);
+function initGame() {
+	game = {
+		players: {},
+		deck: initNewDeck(),
+		turn: 0,
+		currentPlayer: false
+	};
 }
 
+function initNewDeck() {
+	var resp = [];
+
+	for(var i = 3; i <=35; i++) {
+		resp.push(i);
+	}
+	return resp;
+}
+
+function addNewPlayer(name) {
+	if(game.turn > 0) {
+		return false;
+	}
+
+	var player = {
+		cards: [],
+		coins: 11,
+		name: name
+	}
+
+	game.players[name] = player;
+	return player;
+}
